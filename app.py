ad71c927bb55166ddcc5e7fc53650db2765d7b98
Jan Pavlíček <469355@mail.muni.cz>
2024-04-16 00:02:02 +0200

    uptime script, header + footer, charts from db

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ database = mysql.connector.connect(
     database=cfg["database"]["database"],
 )
 
-dashboard_table = cfg["database"]["table"]
+status_table = cfg["database"]["status_table"]
+uptime_table = cfg["database"]["uptime_table"]
 
 
 @app.route('/')
@@ -26,33 +27,10 @@ def home():
     return redirect(url_for('dashboard', selected_range="past_day"))
 
 
-@app.route('/<selected_range>')
-def dashboard(selected_range):  # put application's code here
-    charts_data = [{"service_name": "Auth saml", "ok": 100, "warning": 50, "error": 10},
-                   {"service_name": "Auth saml 2", "ok": 100, "warning": 50,
-                    "error": 15}]
-    status_data = [{"service_name": "auth_saml",
-                    "data": [{"datetime": 11234, "status": "OK"},
-                             {"datetime": 11235, "status": "WARNING"},
-                             {"datetime": 11234, "status": "OK"},
-                             {"datetime": 11234, "status": "OK"},
-                             {"datetime": 11234, "status": "OK"},
-                             {"datetime": 11234, "status": "OK"},
-                             {"datetime": 11234, "status": "OK"},
-                             {"datetime": 11234, "status": "OK"},
-                             {"datetime": 11234, "status": "OK"},
-                             {"datetime": 11234, "status": "OK"},
-                             {"datetime": 11234, "status": "OK"},
-                             {"datetime": 11234, "status": "OK"},
-                             {"datetime": 11234, "status": "OK"},
-                             {"datetime": 11234, "status": "OK"},
-                             {"datetime": 11234, "status": "OK"},
-                             {"datetime": 11234, "status": "OK"},
-                             {"datetime": 112366, "status": "OK"},
-                             {"datetime": 112366, "status": "OK"}, ]},
-                   {"service_name": "auth_saml2"}]
-
+@app.route('/dashboard/<selected_range>')
+def dashboard(selected_range):
     status_data = []
+    charts_data = []
     range_to_sql = {"past_day": "DAY", "past_month": "MONTH", "past_year": "YEAR"}
     if selected_range not in range_to_sql.keys():
         return redirect(url_for('dashboard', selected_range="past_day"))
@@ -62,28 +40,51 @@ def dashboard(selected_range):  # put application's code here
         service_db_name = service["db_name"]
         display_name = service["display_name"]
 
-        db_cursor.execute(f"SELECT * FROM {dashboard_table} WHERE event_time >= NOW() - INTERVAL 1 {range_to_sql[selected_range]} AND service = '{service_db_name}' ORDER BY event_time DESC;;")
-        service_status_data = db_to_dict(db_cursor.fetchall(), display_name)
+        db_cursor.execute(
+            f"SELECT event_time, status FROM {status_table} WHERE event_time >= NOW() - INTERVAL 1 "
+            f"{range_to_sql[selected_range]} AND service = '{service_db_name}' ORDER BY event_time DESC;;")
+        service_status_data = status_db_to_dict(db_cursor.fetchall(), display_name)
         if service_status_data:
             status_data.append(service_status_data)
+
+        db_cursor.execute(
+            f"SELECT uptime, downtime, warntime FROM {uptime_table} "
+            f"WHERE service = '{service_db_name}' AND period = '{range_to_sql[selected_range]}'")
+        service_uptime_data = uptime_db_to_dict(db_cursor.fetchall(), display_name)
+        if service_uptime_data:
+            charts_data.append(service_uptime_data)
+
     db_cursor.close()
     database.commit()
     return render_template('dashboard.html', charts_data=charts_data,
                            status_data=status_data, selected_range=selected_range)
 
 
-def db_to_dict(db_data, display_name):
+def uptime_db_to_dict(db_data, display_name):
+    if not db_data:
+        return {}
+    converted_data = {"service_name": display_name, "OK": 0, "WARNING": 0, "ERROR": 0}
+    for row in db_data:
+        uptime = row[0]
+        downtime = row[1]
+        warntime = row[2]
+        converted_data["OK"] += uptime
+        converted_data["WARNING"] += warntime
+        converted_data["ERROR"] += downtime
+    return converted_data
+
+
+def status_db_to_dict(db_data, display_name):
     if not db_data:
         return {}
     converted_data = {"service_name": display_name, "data": []}
     for row in db_data:
-        event_time = row[3]
-        status = row[4]
+        event_time = row[0]
+        status = row[1]
 
         event = {"datetime": event_time,
                  "status": status}
         converted_data["data"].append(event)
-
     return converted_data
 
 

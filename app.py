2e884b0c137c1b46cc489e0784c78f752a330420
Jan Pavlicek <469355@mail.muni.cz>
2024-04-16 19:23:50 +0200

    select date progress

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-import json
-
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 
 import yaml
 
@@ -20,44 +18,6 @@ database = mysql.connector.connect(
 
 status_table = cfg["database"]["status_table"]
 uptime_table = cfg["database"]["uptime_table"]
-
-
-@app.route('/')
-def home():
-    return redirect(url_for('dashboard', selected_range="past_day"))
-
-
-@app.route('/dashboard/<selected_range>')
-def dashboard(selected_range):
-    status_data = []
-    charts_data = []
-    range_to_sql = {"past_day": "DAY", "past_month": "MONTH", "past_year": "YEAR"}
-    if selected_range not in range_to_sql.keys():
-        return redirect(url_for('dashboard', selected_range="past_day"))
-
-    db_cursor = database.cursor()
-    for service in cfg["services"]:
-        service_db_name = service["db_name"]
-        display_name = service["display_name"]
-
-        db_cursor.execute(
-            f"SELECT event_time, status FROM {status_table} WHERE event_time >= NOW() - INTERVAL 1 "
-            f"{range_to_sql[selected_range]} AND service = '{service_db_name}' ORDER BY event_time DESC;;")
-        service_status_data = status_db_to_dict(db_cursor.fetchall(), display_name)
-        if service_status_data:
-            status_data.append(service_status_data)
-
-        db_cursor.execute(
-            f"SELECT uptime, downtime, warntime FROM {uptime_table} "
-            f"WHERE service = '{service_db_name}' AND period = '{range_to_sql[selected_range]}'")
-        service_uptime_data = uptime_db_to_dict(db_cursor.fetchall(), display_name)
-        if service_uptime_data:
-            charts_data.append(service_uptime_data)
-
-    db_cursor.close()
-    database.commit()
-    return render_template('dashboard.html', charts_data=charts_data,
-                           status_data=status_data, selected_range=selected_range)
 
 
 def uptime_db_to_dict(db_data, display_name):
@@ -81,19 +41,67 @@ def status_db_to_dict(db_data, display_name):
     for row in db_data:
         event_time = row[0]
         status = row[1]
+        host = row[2]
 
-        event = {"datetime": event_time,
-                 "status": status}
+        event = {"datetime": event_time.strftime("%m/%d/%Y %H:%M:%S"),
+                 "status": status,
+                 "host": host}
         converted_data["data"].append(event)
     return converted_data
 
 
-@app.route('/status_data/<service_name>/<index>', methods=["GET"])
-def get_status_data(service_name, index):
-    status_data = {"auth_saml": [{"datetime": 11234, "status": "OK"},
-                                 {"datetime": 11235, "status": "WARNING"}],
-                   "auth_saml2": [{"datetime": 11234, "status": "WARNING"}]}
-    return json.dumps(status_data[service_name])
+def calculate_status_data_for_date(date, cursor):
+    pass
+
+
+@app.route('/')
+def home():
+    return redirect(url_for('dashboard', selected_range="past_day"))
+
+
+@app.route('/dashboard/<selected_range>')
+def dashboard(selected_range):
+    status_data = []
+    charts_data = []
+
+    if selected_range == "select_date":
+        date = request.args.get("date")
+        if date is None:
+            return render_template("dashboard.html", selected_range="select_date",
+                                   charts_data=charts_data, status_data=status_data)
+
+        return render_template('dashboard.html', selected_range="select_date",
+                               charts_data=charts_data, status_data=status_data, )
+
+    range_to_sql = {"past_day": "DAY", "past_month": "MONTH", "past_year": "YEAR"}
+    if selected_range not in range_to_sql.keys():
+        return redirect(url_for('dashboard', selected_range="past_day"))
+
+    db_cursor = database.cursor()
+    db_cursor.execute(f"SELECT DISTINCT service FROM {status_table};")
+    for row in db_cursor.fetchall():
+        service_db_name = row[0]
+        display_name = service_db_name if cfg["services"].get(
+            service_db_name) is None else cfg["services"][service_db_name]
+
+        db_cursor.execute(
+            f"SELECT event_time, status, host FROM {status_table} WHERE event_time >= NOW() - INTERVAL 1 "
+            f"{range_to_sql[selected_range]} AND service = '{service_db_name}' ORDER BY event_time DESC;;")
+        service_status_data = status_db_to_dict(db_cursor.fetchall(), display_name)
+        if service_status_data:
+            status_data.append(service_status_data)
+
+        db_cursor.execute(
+            f"SELECT uptime, downtime, warntime FROM {uptime_table} "
+            f"WHERE service = '{service_db_name}' AND period = '{range_to_sql[selected_range]}'")
+        service_uptime_data = uptime_db_to_dict(db_cursor.fetchall(), display_name)
+        if service_uptime_data:
+            charts_data.append(service_uptime_data)
+
+    db_cursor.close()
+    database.commit()
+    return render_template('dashboard.html', charts_data=charts_data,
+                           status_data=status_data, selected_range=selected_range)
 
 
 if __name__ == '__main__':
